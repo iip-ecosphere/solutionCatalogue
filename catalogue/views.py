3c7010b11d63378248731fbb700b0b90f185f7f5
Sergej Wildemann <wildemann@l3s.de>
2020-12-08 21:36:10 +0100

    cleanup

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,5 +1,3 @@
-from django.http import HttpResponse
-from django.shortcuts import render
 from django.views import generic
 from .models import Component
 from django.contrib.auth.forms import UserCreationForm
@@ -7,19 +5,14 @@ from django.urls import reverse_lazy
 
 
 class IndexView(generic.ListView):
+    queryset = Component.objects.filter(published=True)
     template_name = "catalogue/index.html"
     context_object_name = "components"
 
-    def get_queryset(self):
-        return Component.objects.filter(published=True)
-
 
 class DetailView(generic.DetailView):
-    model = Component
+    queryset = Component.objects.filter(published=True)
     template_name = "catalogue/detail.html"
-
-    def get_queryset(self):
-        return self.model.objects.filter(published=True)
 
 
 class RegisterView(generic.CreateView):
