cd18497846b2e20e6abacfd0ffc5747257f6aada
Sergej Wildemann <wildemann@l3s.de>
2020-12-09 13:44:14 +0100

    use .env for secrets

--- iip_ai_solutions/settings.py
+++ iip_ai_solutions/settings.py
@@ -9,18 +9,23 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load .env
+dotenv_file = BASE_DIR / ".env"
+load_dotenv(dotenv_path=dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "o*^&3w%kvtnz(fwd+5(rszb*loj*ky4y1mf2+6qf@_(gyuhr^v"
+SECRET_KEY = os.getenv("SECRET_KEY", "insecurekey")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
