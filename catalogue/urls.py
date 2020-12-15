4c7870d192818548ecbb2d38bd7e173cb1179fa0
Sergej Wildemann <wildemann@l3s.de>
2020-12-15 15:07:31 +0100

    urls: redirect admin/login to frontend login page

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 from . import views
 
@@ -9,6 +10,7 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("search/", views.SearchView.as_view(), name="search"),
     path("component/<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("admin/login/", RedirectView.as_view(url="/account/login/")),
     path(
         "account/login/",
         auth_views.LoginView.as_view(template_name="catalogue/registration/login.html"),
