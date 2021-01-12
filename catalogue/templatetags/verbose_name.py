245d50d3ec23e54d9e20bf6469a47cdf7ca2cae6
Sergej Wildemann <wildemann@l3s.de>
2021-01-12 15:01:03 +0100

    templates/detail: remove hardcoded field names

--- catalogue/templatetags/verbose_name.py
+++ catalogue/templatetags/verbose_name.py
@@ -4,15 +4,21 @@ register = template.Library()
 
 
 @register.filter
-def verbose_name(object):
-    return object._meta.verbose_name
+def verbose_name(value):
+    if hasattr(value, 'model'):
+        value = value.model
+    return value._meta.verbose_name
 
 
 @register.filter
-def verbose_name_plural(object):
-    return object._meta.verbose_name_plural
+def verbose_name_plural(value):
+    if hasattr(value, 'model'):
+        value = value.model
+    return value._meta.verbose_name_plural
 
 
 @register.simple_tag
 def get_verbose_field_name(instance, field_name):
+    if hasattr(instance, 'model'):
+        instance = instance.model
     return instance._meta.get_field(field_name).verbose_name
