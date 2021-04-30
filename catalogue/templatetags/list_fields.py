0b8ad1eb2a520f17c69f8fa732db4e4bde214ee2
Sergej Wildemann <wildemann@l3s.de>
2021-04-30 20:04:18 +0200

    templates/detail: include related fields in listing

--- catalogue/templatetags/list_fields.py
+++ catalogue/templatetags/list_fields.py
@@ -1,17 +1,23 @@
 from django import template
+from django.db import models
 
 register = template.Library()
 
 
 @register.simple_tag
 def list_fields(object):
+    """Extract all field names and values for an object."""
     for field in object._meta.get_fields():
         if field.auto_created or field.is_relation:
             continue
-        if field.choices:
+        elif field.choices:
             choice = [
                 c for i, c in field.choices if i == field.value_from_object(object)
             ][0]
             yield field.verbose_name, choice
         else:
             yield field.verbose_name, field.value_to_string(object)
+    # display multi-value after single
+    for field in object._meta.get_fields():
+        if isinstance(field, models.ForeignObjectRel):
+            yield field.related_model._meta.verbose_name, getattr(object, field.name + "_set").all()
