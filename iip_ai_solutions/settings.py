a9d0debda9d75285e662a653c10f59251c89c5ff
Sergej Wildemann <wildemann@l3s.de>
2021-06-10 13:41:11 +0200

    settings: add localhost to allowed dev hosts

--- iip_ai_solutions/settings.py
+++ iip_ai_solutions/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "insecurekey")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("SECRET_KEY") is None
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = DEBUG
