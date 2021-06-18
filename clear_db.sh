33f0cc656836f95d592687d8ce3713223c8a2e04
Paul <paul_heinemeyer@outlook.de>
2021-06-18 13:30:24 +0200

    Fixture: removed makemigrations from setup file

--- clear_db.sh
+++ clear_db.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -eo pipefail
-rm -f catalogue/migrations/0*.py
 rm -f db.sqlite3
-python manage.py makemigrations
 python manage.py migrate
 python manage.py initgroups
 python manage.py loaddata superuser users components
