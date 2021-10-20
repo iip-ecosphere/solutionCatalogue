1d4f57820b516d4ffd6f1958949ebe4ba5750b38
Sergej Wildemann <wildemann@l3s.de>
2021-10-20 10:45:49 +0200

    settings: ssl redirect env config

--- iip_ai_solutions/settings.py
+++ iip_ai_solutions/settings.py
@@ -32,7 +32,7 @@ DEBUG = os.getenv("SECRET_KEY") is None
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
-SECURE_SSL_REDIRECT = False
+SECURE_SSL_REDIRECT = bool(os.getenv("SECURE_SSL_REDIRECT", False))
 SESSION_COOKIE_SECURE = DEBUG
 SESSION_SAVE_EVERY_REQUEST = True
 
